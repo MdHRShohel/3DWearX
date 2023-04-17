593136b891e623ca3a4df7cadc1a795edb0fb593
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2023-04-17 06:39:57 +0600

    HomePage added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
+import Canvas from "./canvas/index";
+
 function App() {
 
   return (
-    <div>
-      <h1 className="head-text">React App</h1>
-    </div>
+    <main className='app transition-all ease-in'>
+      <Home/>
+      <Canvas/>
+      <Customizer/>
+    </main>
   )
 }
 
