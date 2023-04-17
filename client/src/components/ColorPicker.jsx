b49791f49ca0d3575788b85178782e5cb1ff1612
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2023-04-18 05:32:18 +0600

    filepicker and color picker updated

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { SketchPicker } from 'react-color';
+import {useSnapshot } from 'valtio';
+
+import state from '../store';
 
 const ColorPicker = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>ColorPicker</div>
+    <div className='absolute left-full ml-3'>
+      <SketchPicker
+      color={snap.color}
+      disableAlpha
+      onChangeComplete={(color) => state.color = color.hex}
+      presetColors={[
+        '#000000',
+        '#ffffff',
+        '#ff0000',
+        '#00ff00',
+        '#0000ff',
+        '#ffff00',
+        '#00ffff',
+        '#ff00ff',
+        '#ff8000',
+        '#ff0080',
+        '#8000ff',
+        '#0080ff',
+        '#008080',
+        '#800080',
+        '#808000',
+        '#808080',
+      ]}
+      />
+    </div>
   )
 }
 
