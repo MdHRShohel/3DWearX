e3e87014033367fe2830d33ec8880b8f7c2152ab
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2023-04-17 07:14:34 +0600

    basic layout of Customizer

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,14 @@
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
 import CustomButton from "./CustomButton";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
 
-export { CustomButton };
+export {
+    CustomButton,
+    AIPicker,
+    ColorPicker,
+    FilePicker,
+    Tab
+};
+
