a689cc3990f1d9a82fb7575b80279ab84e95ad9e
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2023-05-11 02:04:59 +0600

    server init

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,14 +1,12 @@
-import React, { useState, useEffect } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
+import React, { useState } from 'react';
 import { useSnapshot } from 'valtio';
 
-import config from '../config/config';
-import state from '../store';
-import { download } from '../assets';
-import { downloadCanvasToImage, reader } from '../config/helpers';
-import { EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
-import { fadeAnimation, slideAnimation } from '../config/motion';
 import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
+import { DecalTypes, EditorTabs, FilterTabs } from '../config/constants';
+import { reader } from '../config/helpers';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+import state from '../store';
 
 const Customizer = () => {
   const snap = useSnapshot(state);
